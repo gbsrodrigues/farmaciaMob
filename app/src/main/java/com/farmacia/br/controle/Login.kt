b055ea4e94440b90838899fd8f9e1a6a133e345b
Gabriel Rodrigues <gbsrodrigues123>
2019-11-06 18:02:23 -0200

    primeiro commit

---
+++ app/src/main/java/com/farmacia/br/controle/Login.kt
@@ -0,0 +1,13 @@
+package com.farmacia.br.controle
+
+import android.app.Activity
+import android.os.Bundle
+import com.farmacia.br.R
+
+class Login : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
